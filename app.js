83193562efaf7aae183b780778b98ae99f7eb72a
Piotr Olejarz <olejarz-piotr@o2.pl>
2021-10-16 23:56:14 +0200

    creating routes

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const app=express();
+const tasks = require('./routes/tasks');
+
+
+//midleware
+
+app.use(express.json())
+
+app.get('/hello',(req,res)=>{
+
+    res.send('Task manager app')
+})
+
+app.use('/api/v1/tasks',tasks)
 
 
 const port=3000;
